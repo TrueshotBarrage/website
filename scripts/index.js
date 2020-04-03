0bcb18d3aee6996c08d30b6dfd865f15c4df394b
David Kim <jk2537@cornell.edu>
2020-04-03 01:03:31 -0500

    1.2: Small and big screen diff
    - Depending on screen size, header/nav behave differently
    - Implemented jQuery script for sliding left->right or top->bottom

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
-  $nav = $("nav");
+  var $nav = $("nav");
+  var leftToRight = checkSize();
 
   // Toggles the nav bar when clicking the nav-intersect icon.
   $("#nav-intersect").click(() => {
-    $nav.animate({ width: 'toggle' }, 350);
-  })
+    if (leftToRight) {
+      $nav.animate({ width: 'toggle' }, 350); // Big screens
+    } else {
+      $nav.slideToggle(350); // Small screens
+    }
+  });
+
+  $(window).resize(() => {
+    leftToRight = checkSize();
+  });
 });
+
+// If screen size is 500px or greater, return true
+function checkSize() {
+  return $("nav").css("--small") != 1;
+}
