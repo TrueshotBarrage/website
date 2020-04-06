23e960da900a9f509277056668621d5bc9a007f6
David Kim <jk2537@cornell.edu>
2020-04-06 18:55:15 -0500

    2.1.2: Minor changes (the bandage treatment)
    - Projects placeholder slightly modified for better page display
    - GitHub link added for now
    - Main page text now includes padding

--- projects.php
+++ projects.php
@@ -44,6 +44,14 @@ $title = "projects";
       <div class="coming-soon contents">
         <h2>Projects section coming soon! Currently redesigning the entire
           website, so hang tight during this COVID-19 season. Good things are coming.</h2>
+        <h3>For now, check out my full list of projects on my
+          <a href="https://github.com/TrueshotBarrage" target="_blank">
+            <span class="rainbow">~GitHub~</span>
+          </a>
+        </h3>
+        <a href="https://github.com/TrueshotBarrage" target="_blank">
+          <img src="images/github.png" />
+        </a>
       </div>
 
       <!-- Footer -->
