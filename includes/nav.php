d6926314989f555310ce2be4e16adcc8dafbdb9a
David Kim <jk2537@cornell.edu>
2020-04-08 00:23:32 -0500

    3.1.4: Minor retouches to navbar/profile
    - Nav bar(s) links are black and have more margin between
    - Profile: skills section right side font decreased

--- includes/nav.php
+++ includes/nav.php
@@ -5,13 +5,13 @@
     </a>
   </li>
   <li>
-    <a href="projects.php">
-      <span <?php echo ($title == "projects") ? "class=\"xx\"" : "" ?>>Projects</span>
+    <a href="profile.php">
+      <span <?php echo ($title == "about me") ? "class=\"xx\"" : "" ?>>About Me</span>
     </a>
   </li>
   <li>
-    <a href="profile.php">
-      <span <?php echo ($title == "about me") ? "class=\"xx\"" : "" ?>>About Me</span>
+    <a href="projects.php">
+      <span <?php echo ($title == "projects") ? "class=\"xx\"" : "" ?>>Projects</span>
     </a>
   </li>
   <li>
