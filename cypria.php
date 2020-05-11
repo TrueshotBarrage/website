f9c309e718bd164b4a37b41fe2c64bcbaf7af647
David Kim <jk2537@cornell.edu>
2020-05-11 01:13:35 -0500

    3.4: Added Cypria to projects

--- cypria.php
+++ cypria.php
@@ -1,46 +1,7 @@
 <?php
 include("includes/init.php");
 
-$title = "projects";
-
-function create_project_entries($p, $l)
-{
-  $sizes = array("medium", "small", "big", "small", "big", "medium", "big", "small", "medium");
-  $colors = array("#E9D2D2", "#ACD9F2", "#E1D4E8", "#FFC09A", "#E2F8BD", "#C8CDF0", "#B1F2EC", "#FFCF99", "#F3EAED");
-  for ($i = 0; $i < count($p); $i++) { ?>
-    <div class="contents project hidden <?php echo $sizes[$i % 9]; ?>" id="p<?php echo ($i + 1); ?>">
-      <div class="container" <?php echo " style=\"background-color:" . $colors[$i % 9] . "\"" ?>>
-        <a href="<?php echo $l[$i] ?>">
-          <div class="inner-container">
-            <h2><?php echo $p[$i]; ?></h2>
-            <img data-src="images/projects/p<?php echo ($i + 1); ?>.png" />
-          </div>
-        </a>
-      </div>
-    </div>
-<?php }
-}
-
-$projects = array(
-  "Project Tempo",
-  "MIDI Visualizer",
-  "CypriaQL",
-  "LED Tetris",
-  "Crawl-o-Bot",
-  "StudyBuddy",
-  "Sir Mix-A-Lot",
-  "GPA Calculator",
-);
-$links = array(
-  "project-tempo.php",
-  "midi-visualizer.php",
-  "cypria.php",
-  "led-tetris.php",
-  "https://github.com/TrueshotBarrage/crawl-o-bot",
-  "https://github.com/TrueshotBarrage/studybuddy",
-  "sir-mix-a-lot.php",
-  "https://docs.google.com/spreadsheets/d/13gFiOqcAMy0oDZBiwmrRTxWc00Ag2eL9FyFD5U3VnsA/",
-);
+$title = "cypria";
 ?>
 
 <!DOCTYPE html>
@@ -56,7 +17,6 @@ $links = array(
   <link rel="icon" href="images/favicon.ico" type="image/x-icon">
   <!-- CSS -->
   <link rel="stylesheet" type="text/css" href="css/main.css">
-  <link rel="stylesheet" type="text/css" href="css/projects.css">
   <link rel="stylesheet" type="text/css" href="css/nav-header.css">
   <!-- Scripts -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -64,6 +24,11 @@ $links = array(
   <script type="text/javascript" src="scripts/main.js"></script>
   <?php include("includes/gtm.php"); ?>
   <!-- Keywords, names, properties go here later -->
+  <!-- Redirect for now -->
+  <meta http-equiv="refresh" content="0; url=http://cypria-frontend.herokuapp.com">
+  <script type="text/javascript">
+    window.location.href = "http://cypria-frontend.herokuapp.com"
+  </script>
 </head>
 
 <body>
@@ -86,11 +51,11 @@ $links = array(
   </div>
 
   <div class="black-bg">
+    <!-- The main contents: greeting, intro texts, ext. links, and footer -->
     <div class="main-container">
-      <div class="projects">
-        <?php create_project_entries($projects, $links); ?>
-      </div>
 
+      If you are not redirected automatically, follow this
+      <a href="http://cypria-frontend.herokuapp.com">link to the website</a>!
       <!-- Footer -->
       <footer>
         <?php include("includes/footer.php"); ?>
