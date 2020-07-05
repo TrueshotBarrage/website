4977ae57dc4f176324d250e5ad59d82e4703788c
David Kim <46143688+TrueshotBarrage@users.noreply.github.com>
2020-07-05 04:32:38 -0500

    4.1.1: Finished half of the resume

--- resume.php
+++ resume.php
@@ -2,6 +2,25 @@
 include("includes/init.php");
 
 $title = "resume";
+
+function work_gen($company, $role, $datestr, $location, $description_array)
+{ ?>
+  <div class="project-title">
+    <span class="bold"><?php echo $company; ?> &#183;</span>
+    <?php echo $role; ?>
+  </div>
+  <div class="subtitle">
+    <span class="unset"><?php echo $datestr; ?>&#8192;&#9702;</span>
+    &#8192;<?php echo $location; ?>
+  </div>
+  <ul>
+    <?php foreach ($description_array as $description) { ?>
+      <li><?php echo $description; ?></li>
+    <?php } ?>
+  </ul>
+  <div class="spaced"></div>
+<?php
+}
 ?>
 
 <!DOCTYPE html>
@@ -72,6 +91,90 @@ $title = "resume";
         <a href="https://www.github.com/TrueshotBarrage">
           <img src="images/resume/github.svg" />TrueshotBarrage
         </a>
+      </div>
+    </div>
+
+    <div class="contents">
+      <div class="left-column">
+        <div id="education">
+          <div class="section-title">Education</div>
+          <div id="school"><span class="bold bigger">Cornell University</span>
+            <span> &#183; Aug. '18 - May '22</span></div>
+          <div>B.S. Computer Science</div>
+          <div>B.S. Electrical and Computer Engineering</div>
+          <div>GPA: 3.575</div>
+        </div>
+        <div id="skills">
+          <div class="section-title">Skills</div>
+          <div class="bold">Programming Languages:</div>
+          <div class="subtitle">Proficient with</div>
+          <div>Java, Python, JavaScript, PHP, OCaml, Go</div>
+          <div class="subtitle">Have experience with</div>
+          <div class="spaced">TypeScript, C, Verilog, Ruby, Swift</div>
+          <div><span class="bold">Other: </span>Git, Node.js,
+            Linux (Ubuntu/WSL), Docker, Swagger, Grafana, ClickHouse</div>
+        </div>
+        <div id="selected-projects">
+          <div class="section-title">Selected Projects</div>
+          <div class="project-title">MIDI Visualizer</div>
+          MIDI file parser. Synthesizes sound and generates real-time piano
+          keypress effects using JavaScript.
+          <div class="project-title">Cypria</div>
+          Programming language: developed in OCaml, interprets to SQL.
+          Headache-proofs complicated queries with functional constructs:
+          map and reduce.
+          <div class="project-title">Project Tempo</div>
+          Algorithm-driven, music-composing Python program that randomly
+          generates "good" music.
+          <div class="project-title">LED Tetris</div>
+          Full LED Tetris game for the Arduino Mega 2560. Organized with a
+          finite state machine.
+          <div class="project-title">Crawl-o-Bot</div>
+          Data scraper in PHP that decides where to crawl using a
+          randomized DFS algorithm.
+          <div class="project-title">The resume you're looking at</div>
+          Completely custom built with lots and lots of CSS grid.
+          No satisfaction guarantee with Internet Explorer.
+        </div>
+      </div>
+      <div class="right-column">
+        <div id="work-experience">
+          <div class="section-title">Relevant Employment</div>
+          <div class="project-title">
+            <span class="bold">Wasabi Technologies, Inc. &#183;</span>
+            SWE Intern
+          </div>
+          <div class="subtitle">
+            <span class="unset">May 2020 - Current&#8192;&#9702;</span>&#8192;Boston, MA
+          </div>
+          <ul>
+            <li>
+              Developed Swagger API to generate server backend for company CDR
+              data using Go.
+            </li>
+            <li>
+              Generated over twenty time-series charts with CDR data by
+              writing optimized ClickHouse SQL queries on Grafana.
+            </li>
+          </ul>
+          <div class="spaced"></div>
+          <?php work_gen(
+            "Cornell Engineering",
+            "TA",
+            "Jan. 2019 - Dec. 2019",
+            "Ithaca, NY",
+            array(
+              "ECE 2300 - Digital Logic & Computer Org.",
+              "PHYS 1112 - Mechanics & Heat"
+            )
+          ); ?>
+        </div>
+        <div id="extracurriculars">
+          <div class="section-title">Extracurriculars</div>
+        </div>
+        <div id="relevant-coursework">
+          <div class="section-title">Relevant Coursework</div>
+        </div>
       </div>
     </div>
   </div>
